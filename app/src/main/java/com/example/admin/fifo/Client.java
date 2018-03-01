71128c80991fe2e396c8fef7764a6bbbd1cda2d2
Tara <taray001@gmail.com>
2018-03-01 12:53:16 -0800

    Added functions of Client and Server

---
+++ app/src/main/java/com/example/admin/fifo/Client.java
@@ -0,0 +1,61 @@
+package com.example.admin.fifo;
+import com.example.admin.fifo.Server;
+
+/**
+ * Created by kenny on 2/24/2018.
+ */
+
+
+public class Client{
+    public final static int SEC_PER_CUSTOMER = 60;
+
+    String clientName;              // the name of the customers
+    int position;                   // customer position in line (1 meaning the custmer is 1st in line)
+    int secondsWaited;              // how long the customer has waited
+    int secondsRemaining;           // an estimate of how long the customer has until checkout
+    String storeCode;               // the code customers enter to join a store
+
+    /* Constructor */
+    Client(String name, String code) {      // CONSTRUCTOR
+        clientName = name;
+        storeCode = code;
+
+    }
+
+    Client(String name) {
+        clientName = name;
+    }
+
+    /* FUNCTIONS */
+    void leave() {
+        // ask the server to remove the client from the queue
+        storeCode = "";                 // "" indicates is not currently waiting in a queue
+
+    }
+
+    int getPosition() {
+        return position;
+    }
+
+    void advancePosition() {
+        position = position + 1;
+    }
+
+    int getSecondsWaited() {
+        // Clock since added to line???
+        return secondsWaited;
+    }
+
+    int getSecondsRemaining() {
+        secondsRemaining = position * 60;
+        return  secondsRemaining;
+    }
+
+    String getStoreCode() {
+        return storeCode;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/admin/fifo/Server.java
@@ -0,0 +1,64 @@
+package com.example.admin.fifo;
+import com.example.admin.fifo.Client;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
+/**
+ * Created by kenny on 2/24/2018.
+ */
+
+public class Server {
+    String ServerName;              // the name of the the store
+    Queue<Client> LINE;             // the queue of clients
+    Client ProcessingClient;        // the client that is currently being serviced
+    boolean isClient;               // there is a client in ProcessingClient / server is serving someone
+
+    /* Constructor */
+    Server(String name) {
+        ServerName = name;
+        LINE = new LinkedList<Client>();
+        isClient = false;
+    }
+
+    /* FUNCTIONS */
+     boolean getClient() {
+        try {
+            ProcessingClient = LINE.remove();
+        }
+        catch (NoSuchElementException e) {
+            isClient = false;
+            return false;
+        }
+        isClient = true;
+        return true;
+    }
+
+    boolean getEmpty() {
+         return LINE.isEmpty();
+    }
+
+    boolean nextClient() {
+         if(getClient()) {
+             return true;
+         }
+         return false;
+    }
+
+    void kickToEnd() {
+        //Put Client at end of queue
+        LINE.add(ProcessingClient);
+        nextClient();
+    }
+
+    boolean kickOut() {
+         return nextClient();
+    }
+
+    boolean servicingClient() {
+         return isClient;
+    }
+
+
+}
